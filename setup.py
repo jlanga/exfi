0d930d77e311988788da93f1f20cf95a54768a41
Jorge Langa <jorge.langa.arranz@gmail.com>
2017-03-27 17:45:13 +0200

    improved find_exons. modified setup.py to get the name change

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup
 
 setup(
     name='exfi',
-    version='0.1dev',
+    version='0.1dev0',
     packages=['exfi'],
     license='MIT',
     install_requires=[
@@ -12,6 +12,12 @@ setup(
     long_description=open('README.rst').read(),
     test_suite='nose.collector',
     tests_require=['nose'],
+    entry_points={
+          'console_scripts': ['funniest-joke=funniest.command_line:main'],
+      },
+    scripts = [
+        "bin/find_exons",
+    ],
     include_package_data=True,
     zip_safe=False
 )

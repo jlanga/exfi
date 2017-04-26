5e4d0459cb0c0eaf5e6153fe7ee07b82eca908c6
Jorge Langa <jorge.langa.arranz@gmail.com>
2017-04-26 11:30:33 +0200

    Cleaning up

--- exfi/filter_by_length.py
+++ exfi/filter_by_length.py
@@ -3,7 +3,3 @@
 def filter_by_length(iterables, length):
     """Filter each of the elements in iterables by length"""
     return (element for element in iterables if len(element.seq) >= length)
-
-    # for element in iterables:
-    #     if len(element.seq) >= length:
-    #         yield element

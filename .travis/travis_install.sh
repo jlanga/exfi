621c6c6ef12b14947b897991b1468c8bc4c9f1a2
Jorge Langa <jorge.langa.arranz@gmail.com>
2017-05-29 23:08:10 +0200

    Improved .travis.yml

---
+++ .travis/travis_install.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/env bash
+# Conda packages
+conda config --add channels conda-forge
+conda config --add channels defaults
+conda config --add channels r
+conda config --add channels bioconda
+conda install --yes abyss bedtools biopython
+
+# Manually installed repos
+git clone https://github.com/bcgsc/biobloom src/biobloom
+pushd src/biobloom
+./autogen.sh && ./configure && make -j 2 && sudo make install -j 2
+popd

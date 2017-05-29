95d929ee9f403242926157f6831e73a7d4af6067
Jorge Langa <jorge.langa.arranz@gmail.com>
2017-05-29 18:32:56 +0200

    Merged installers

--- .travis/install.sh
+++ .travis/install.sh
@@ -1,7 +1,25 @@
 #!/bin/bash
 
 if [[ $TRAVIS_OS_NAME == 'osx' ]]; then
-    bash ./travis/install_osx.sh
+    brew tap homebrew/science
+    brew install biobloom curl
+    curl -O https://repo.continuum.io/miniconda/Miniconda3-latest-MacOSX-x86_64.sh
+    bash Miniconda3-latest-Linux-x86_64.sh -b
 else
-    bash ./travis/install_linux.sh
+    sudo apt-get -qq update
+    sudo apt-get install -y build-essential curl git libboost-dev
+    curl -O https://repo.continuum.io/miniconda/Miniconda3-latest-Linux-x86_64.sh
+    bash Miniconda3-latest-Linux-x86_64.sh -b
+    git clone https://github.com/bcgsc/biobloom src/biobloom
+    pushd src/biobloom
+    ./autogen.sh && ./configure && make install -j 2
+    popd
+
 fi
+
+export PATH="/home/travis/miniconda3/bin:$PATH"
+conda config --add channels conda-forge
+conda config --add channels defaults
+conda config --add channels r
+conda config --add channels bioconda
+conda install --yes abyss bedtools biopython

9790c9ccf45b506a39269cbc09318d0c4336d797
Jorge Langa <jorge.langa.arranz@gmail.com>
2017-05-30 11:41:50 +0200

    Installing gcc via brew

--- .travis/travis_install.sh
+++ .travis/travis_install.sh
@@ -8,6 +8,12 @@ conda config --add channels r
 conda config --add channels bioconda
 conda install --yes abyss bedtools biopython
 
+
+# Install GCC and boost via brew
+if [[ $TRAVIS_OS_NAME == 'osx' ]]; then
+    brew install gcc boost
+fi
+
 # Biobloomtools
 git clone https://github.com/bcgsc/biobloom ~/download/biobloom
 pushd ~/download/biobloom

908fa2ec7e5da9c44fba5a4ee6b965530fe6ddda
Jorge Langa <jorge.langa.arranz@gmail.com>
2017-06-01 00:09:37 +0200

    pep8

--- exfi/build_baited_bloom_filter.py
+++ exfi/build_baited_bloom_filter.py
@@ -1,5 +1,6 @@
 #!/usr/bin/env python3
 
+
 def build_baited_bloom_filter(
     transcriptome,
     kmer,
@@ -13,7 +14,8 @@ def build_baited_bloom_filter(
 
     Run the build_baited_bloom_filter pipeline:
     - Build a secondary Bloom filter of the transcriptome with biobloommaker
-    - Categorize reads with biobloomcategorizer and pipe to abyss-bloom build  to build the primary Bloom filter
+    - Categorize reads with biobloomcategorizer and pipe to abyss-bloom build
+        to build the primary Bloom filter
 
     Inputs are:
     - A transcriptome in Fasta format
@@ -37,39 +39,39 @@ def build_baited_bloom_filter(
     # Prepare the commands
     build_transcriptome_bf = [
         'biobloommaker',
-            '--file_prefix', "transcriptome",
-            '--output_dir', output_dir,
-            '--threads', str(threads),
-            '--kmer_size', str(kmer),
-            transcriptome
+        '--file_prefix', "transcriptome",
+        '--output_dir', output_dir,
+        '--threads', str(threads),
+        '--kmer_size', str(kmer),
+        transcriptome
     ]
 
     categorize = [
         'biobloomcategorizer',
-            '--prefix', output_dir + '/categories',
-            '--filter_files', output_dir + '/transcriptome.bf',
-            '--threads', str(threads),
-            '--score', str(kmer),
-            '--fa',
-            '--stdout_filter', 'transcriptome',
-            *reads
+        '--prefix', output_dir + '/categories',
+        '--filter_files', output_dir + '/transcriptome.bf',
+        '--threads', str(threads),
+        '--score', str(kmer),
+        '--fa',
+        '--stdout_filter', 'transcriptome',
+        *reads
     ]
 
     build_bf = [
         'abyss-bloom', 'build',
-            '--verbose',
-            '--kmer', str(kmer),
-            '--bloom-size', bloom_size,
-            '--levels', str(levels),
-            '--threads', str(threads),
-            output_bloom,
-            '/dev/stdin'
+        '--verbose',
+        '--kmer', str(kmer),
+        '--bloom-size', bloom_size,
+        '--levels', str(levels),
+        '--threads', str(threads),
+        output_bloom,
+        '/dev/stdin'
     ]
 
     # Run the pipeline
     stderr.write(
         "\nRunning command: {command}\n".format(
-            command= " ".join(build_transcriptome_bf)
+            command=" ".join(build_transcriptome_bf)
         )
     )
 
@@ -79,17 +81,15 @@ def build_baited_bloom_filter(
 
     p_build_transcriptome_bf.wait()
 
-    #p_build_transcriptome_bf.communicate()
-
     stderr.write(
         "\nRunning commands: {command1} | {command2}\n".format(
-            command1 = " ".join(categorize),
-            command2 = " ".join(build_bf)
+            command1=" ".join(categorize),
+            command2=" ".join(build_bf)
         )
     )
 
-    p_categorize = Popen(categorize, stdout = PIPE)
-    p_build_bf = Popen(build_bf, stdin = p_categorize.stdout)
+    p_categorize = Popen(categorize, stdout=PIPE)
+    p_build_bf = Popen(build_bf, stdin=p_categorize.stdout)
 
     p_categorize.stdout.close()
     p_categorize.wait()

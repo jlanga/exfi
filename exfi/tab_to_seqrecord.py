b2a15462eca500d2157ea182fcf48f649b25ef4f
Jorge Langa <jorge.langa.arranz@gmail.com>
2017-06-01 00:24:56 +0200

    pep8

--- exfi/tab_to_seqrecord.py
+++ exfi/tab_to_seqrecord.py
@@ -1,7 +1,9 @@
 #!/usr/bin/env python3
 
+
 from Bio.SeqRecord import SeqRecord
 from Bio.Seq import Seq
+
 
 def tab_to_seqrecord(iterable):
     for element in iterable:
@@ -10,7 +12,7 @@ def tab_to_seqrecord(iterable):
             new_identifier = identifier.rsplit(":")[0]
             description = identifier
             yield SeqRecord(
-                id= new_identifier,
-                seq= Seq(sequence),
-                description = description
+                id=new_identifier,
+                seq=Seq(sequence),
+                description=description
             )

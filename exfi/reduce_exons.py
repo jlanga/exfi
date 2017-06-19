06beddda56d7564830d9278ca43a0df6b45d2fd8
Jorge Langa <jorge.langa.arranz@gmail.com>
2017-06-19 17:33:15 +0200

    Simplified and improved ram usage

--- exfi/reduce_exons.py
+++ exfi/reduce_exons.py
@@ -6,61 +6,32 @@ from Bio.SeqRecord import SeqRecord
 from Bio.Seq import Seq
 
 
-def _make_seq_to_exon(iterable_of_seqrecords):
-    # 1. Make an association between the exon sequence and info from
-    # the transcriptome
-    # keys are sequences (as str) and values are
-    # [new_exon_id, old_exon1_id, ..., old_exonN_id]
-    seq_to_exon = dict()  # sequence : [exons_ids with such sequence]
-
-    for record in iterable_of_seqrecords:
-
-        # Split record into relevant info
-        sequence = str(record.seq)
-
-        # Initialize the sequence
-        if str(record.seq) not in seq_to_exon:
-            new_exon_id = 'EXON{:011d}'.format(len(seq_to_exon) + 1)
-            seq_to_exon[sequence] = [new_exon_id]
-
-        # Just add the other info
-        seq_to_exon[sequence].append(record.description)
-    return seq_to_exon
-
-
-def _exonid_to_seqrecord(seq_to_exon):
-    # 2. revert the dictionary and return sequences
-    # Dict has the structure exon_id : seqrecord
-
-    exonid_to_seqrecord = {}
-
-    for sequence in seq_to_exon:
-
-        info = seq_to_exon[sequence]
-
-        identifier = info[0]
-        description = " ".join(info[1:])
-
-        record = SeqRecord(
-            id=identifier,
-            description=description,
-            seq=Seq(sequence)
-        )
-
-        exonid_to_seqrecord[identifier] = record
-    return exonid_to_seqrecord
-
-
 def reduce_exons(iterable_of_seqrecords):
-    """(iterable of SeqRecords) -> iterable of SeqRecords
+    """Reduce the exons by sequence identity
 
-    Reduce redundacy of the iterable seqrecords by removing repeated exons
-    and storing graph info into the description header
+    Build 2 dicts in which keys are the sequences. In one of them store
+    {seq : exon_identifier} and in the other {seq : description} where
+    description is the exon coordinates.
+    Once collected all records, return them.
     """
+    # Initialize
+    seq_to_id = dict()
+    seq_to_description = dict()
 
-    seq_to_exon = _make_seq_to_exon(iterable_of_seqrecords)
-    exonid_to_seqrecord = _exonid_to_seqrecord(seq_to_exon)
+    # Go over each record
+    for record in iterable_of_seqrecords:
+        seq = str(record.seq)
+        if seq in seq_to_id.keys():  # Just append
+            seq_to_description[seq] += " " + record.description
+        else:  # Enter values in both dicts
+            seq_to_description[seq] = record.description
+            seq_to_id[seq] = 'EXON{:011d}'.format(len(seq_to_id) + 1)
 
-    # Return results ordered by exon_id
-    for identifier in sorted(exonid_to_seqrecord):
-        yield exonid_to_seqrecord[identifier]
+    # Collect data from both dicts into a SeqRecord and return
+    for seq in seq_to_id.keys():
+        record = SeqRecord(
+            id=seq_to_id[seq],
+            description=seq_to_description[seq],
+            seq=Seq(seq)
+        )
+        yield record

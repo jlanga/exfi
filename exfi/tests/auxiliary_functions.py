6e31daf181342c9353a75521d060625683f37657
Jorge Langa <jorge.langa.arranz@gmail.com>
2017-08-24 14:22:54 +0200

    Simplified tests

--- exfi/tests/auxiliary_functions.py
+++ exfi/tests/auxiliary_functions.py
@@ -1,47 +1,39 @@
 #!/usr/bin/env python3
 
 """
-Auxiliary functions for testing
+Auxiliary functions and classes for testing
 """
 
-def equal_seqrecord(seqrecord1, seqrecord2):
-    """
-    Check if seqrecord1 and seqrecord2 are identical in terms of:
-    - id
-    - sequence (as a Seq object)
-    """
-    if(seqrecord1.id == seqrecord2.id and seqrecord1.seq == seqrecord2.seq):
-        return True
-    else:
-        return False
+from Bio import SeqIO
 
+class CustomAssertions:
 
-def equal_list_of_seqrecords(list_of_seqrecords1, list_of_seqrecords2):
-    """
-    Check if each element of list_of_seqrecords1 is exactly equal to each one of
-    list_of_seqrecords2.
-    """
-    n1 = len(list_of_seqrecords1)
-    n2 = len(list_of_seqrecords2)
-    if n1 != n2:
-        raise AssertionError(
-            'Lengths differ:\n {len_1} != {len_2}'.format(
-                len_1 = n1,
-                len_2 = n2
-            )
-        )
-    else:
-        for i in range(n1):
-            record1=list_of_seqrecords1[i]
-            record2=list_of_seqrecords2[i]
-            if not equal_seqrecord(record1, record2):
-                raise AssertionError(
-                    'Records at position {i} differ:\n{id1} : {seq1}\n{id2} : {seq2}'.format(
-                        i=i,
-                        id1=record1.id,
-                        seq1=record1.seq,
-                        id2=record2.id,
-                        seq2=record2.seq
-                    )
+    def assertEqualListOfSeqrecords(self, records1, records2):
+        """
+        Check if each element of list_of_seqrecords1 is exactly equal to each one of
+        list_of_seqrecords2.
+        """
+        n1 = len(records1)
+        n2 = len(records2)
+        if n1 != n2:
+            raise AssertionError(
+                'Lengths differ:\n {len_1} != {len_2}'.format(
+                    len_1 = n1,
+                    len_2 = n2
                 )
-        return True
+            )
+        else:
+            for i in range(n1):
+                record1=records1[i]
+                record2=records2[i]
+                if record1.id != record2.id or record1.seq != record2.seq:
+                    raise AssertionError(
+                        'Records at position {i} differ:\n{id1} : {seq1}\n{id2} : {seq2}'.format(
+                            i=i,
+                            id1=record1.id,
+                            seq1=record1.seq,
+                            id2=record2.id,
+                            seq2=record2.seq
+                        )
+                    )
+            return True
--- exfi/tests/test_reduce_exons.py
+++ exfi/tests/test_reduce_exons.py
@@ -4,9 +4,10 @@
 from unittest import TestCase
 from Bio import SeqIO
 from exfi.reduce_exons import reduce_exons
-from exfi.tests.auxiliary_functions import equal_list_of_seqrecords
+from exfi.tests.auxiliary_functions import CustomAssertions
 
-class TestReduceExons(TestCase):
+
+class TestReduceExons(TestCase, CustomAssertions):
 
     def test_empty_sequence(self):
         """reduce_exons.py: test empty fasta"""
@@ -14,12 +15,16 @@ class TestReduceExons(TestCase):
             handle="exfi/tests/files/reduce_exons/empty_sequence_input.fa",
             format="fasta"
         )
-        actual = list(reduce_exons(records))
-        expected = list(SeqIO.parse(
+        actual = reduce_exons(records)
+        expected = SeqIO.parse(
             handle="exfi/tests/files/reduce_exons/empty_sequence_output.fa",
             format="fasta"
-        ))
-        self.assertTrue(equal_list_of_seqrecords(actual, expected))
+        )
+        self.assertEqualListOfSeqrecords(
+            list(actual),
+            list(expected)
+        )
+
 
     def test_one_exon(self):
         """reduce_exons.py: test single exon"""
@@ -27,12 +32,15 @@ class TestReduceExons(TestCase):
             handle="exfi/tests/files/reduce_exons/one_exon_input.fa",
             format="fasta"
         )
-        actual = list(reduce_exons(records))
-        expected = list(SeqIO.parse(
+        actual = reduce_exons(records)
+        expected = SeqIO.parse(
             handle="exfi/tests/files/reduce_exons/one_exon_output.fa",
             format="fasta"
-        ))
-        self.assertTrue(equal_list_of_seqrecords(actual, expected))
+        )
+        self.assertEqualListOfSeqrecords(
+            list(actual),
+            list(expected)
+        )
 
     def test_same_exon(self):
         """reduce_exons.py: read the same exon twice"""
@@ -40,12 +48,15 @@ class TestReduceExons(TestCase):
             handle="exfi/tests/files/reduce_exons/same_exon_input.fa",
             format="fasta"
         )
-        actual = list(reduce_exons(records))
-        expected = list(SeqIO.parse(
+        actual = reduce_exons(records)
+        expected = SeqIO.parse(
             handle="exfi/tests/files/reduce_exons/same_exon_output.fa",
             format="fasta"
-        ))
-        self.assertTrue(equal_list_of_seqrecords(actual, expected))
+        )
+        self.assertEqualListOfSeqrecords(
+            list(actual),
+            list(expected)
+        )
 
 
     def test_different_exons(self):
@@ -54,9 +65,12 @@ class TestReduceExons(TestCase):
             handle="exfi/tests/files/reduce_exons/different_exons_input.fa",
             format="fasta"
         )
-        actual = list(reduce_exons(records))
-        expected = list(SeqIO.parse(
+        actual = reduce_exons(records)
+        expected = SeqIO.parse(
             handle="exfi/tests/files/reduce_exons/different_exons_output.fa",
             format="fasta"
-        ))
-        self.assertTrue(equal_list_of_seqrecords(actual, expected))
+        )
+        self.assertEqualListOfSeqrecords(
+            list(actual),
+            list(expected)
+        )

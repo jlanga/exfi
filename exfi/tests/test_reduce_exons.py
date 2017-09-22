9aef1df93048c940ab63c46dc3fb8d44161a0c6d
Jorge Langa <jorge.langa.arranz@gmail.com>
2017-09-22 16:53:36 +0200

    Reduced clones?

--- exfi/tests/test_reduce_exons.py
+++ exfi/tests/test_reduce_exons.py
@@ -4,75 +4,48 @@
 from unittest import TestCase
 from Bio import SeqIO
 from exfi.reduce_exons import reduce_exons
-from exfi.tests.auxiliary_functions import CustomAssertions
-
+from exfi.tests.auxiliary_functions import \
+    CustomAssertions, \
+    _fasta_to_list
 
 class TestReduceExons(TestCase, CustomAssertions):
 
     @classmethod
     def test_empty_sequence(self):
         """reduce_exons.py: test empty fasta"""
-        records = SeqIO.parse(
-            handle="exfi/tests/files/reduce_exons/empty_sequence_input.fa",
-            format="fasta"
-        )
-        actual = reduce_exons(records)
-        expected = SeqIO.parse(
-            handle="exfi/tests/files/reduce_exons/empty_sequence_output.fa",
-            format="fasta"
-        )
+        fasta_in = "exfi/tests/files/reduce_exons/empty_sequence_input.fa"
+        fasta_res = "exfi/tests/files/reduce_exons/empty_sequence_output.fa"
         self.assertEqualListOfSeqrecords(
-            list(actual),
-            list(expected)
+            list(reduce_exons(_fasta_to_list(fasta_in))),
+            _fasta_to_list(fasta_res)
         )
 
     @classmethod
     def test_one_exon(self):
         """reduce_exons.py: test single exon"""
-        records = SeqIO.parse(
-            handle="exfi/tests/files/reduce_exons/one_exon_input.fa",
-            format="fasta"
-        )
-        actual = reduce_exons(records)
-        expected = SeqIO.parse(
-            handle="exfi/tests/files/reduce_exons/one_exon_output.fa",
-            format="fasta"
-        )
+        fasta_in = "exfi/tests/files/reduce_exons/one_exon_input.fa"
+        fasta_res = "exfi/tests/files/reduce_exons/one_exon_output.fa"
         self.assertEqualListOfSeqrecords(
-            list(actual),
-            list(expected)
+            list(reduce_exons(_fasta_to_list(fasta_in))),
+            _fasta_to_list(fasta_res)
         )
 
     @classmethod
     def test_same_exon(self):
         """reduce_exons.py: read the same exon twice"""
-        records = SeqIO.parse(
-            handle="exfi/tests/files/reduce_exons/same_exon_input.fa",
-            format="fasta"
-        )
-        actual = reduce_exons(records)
-        expected = SeqIO.parse(
-            handle="exfi/tests/files/reduce_exons/same_exon_output.fa",
-            format="fasta"
-        )
+        fasta_in = "exfi/tests/files/reduce_exons/same_exon_input.fa"
+        fasta_res = "exfi/tests/files/reduce_exons/same_exon_output.fa"
         self.assertEqualListOfSeqrecords(
-            list(actual),
-            list(expected)
+            list(reduce_exons(_fasta_to_list(fasta_in))),
+            _fasta_to_list(fasta_res)
         )
 
     @classmethod
     def test_different_exons(self):
         """reduce_exons.py: read the same exon twice"""
-        records = SeqIO.parse(
-            handle="exfi/tests/files/reduce_exons/different_exons_input.fa",
-            format="fasta"
-        )
-        actual = reduce_exons(records)
-        expected = SeqIO.parse(
-            handle="exfi/tests/files/reduce_exons/different_exons_output.fa",
-            format="fasta"
-        )
+        fasta_in = "exfi/tests/files/reduce_exons/different_exons_input.fa"
+        fasta_res = "exfi/tests/files/reduce_exons/different_exons_output.fa"
         self.assertEqualListOfSeqrecords(
-            list(actual),
-            list(expected)
+            list(reduce_exons(_fasta_to_list(fasta_in))),
+            _fasta_to_list(fasta_res)
         )

b77c5c44648cfad14563060e2308f7b44bbbd976
Jorge Langa <jorge.langa.arranz@gmail.com>
2017-09-22 17:05:19 +0200

    Deleted clones?

--- exfi/tests/test_reduce_exons.py
+++ exfi/tests/test_reduce_exons.py
@@ -9,42 +9,42 @@ from exfi.tests.auxiliary_functions import \
 
 class TestReduceExons(TestCase, CustomAssertions):
 
-    @classmethod
     def test_empty_sequence(self):
         """reduce_exons.py: test empty fasta"""
-        fasta_in = "exfi/tests/files/reduce_exons/empty_sequence_input.fa"
-        fasta_res = "exfi/tests/files/reduce_exons/empty_sequence_output.fa"
-        self.assertEqualListOfSeqrecords(
-            list(reduce_exons(_fasta_to_list(fasta_in))),
-            _fasta_to_list(fasta_res)
+        actual = list(reduce_exons(_fasta_to_list(
+            "exfi/tests/files/reduce_exons/empty_sequence_input.fa"
+        )))
+        expected = _fasta_to_list(
+            "exfi/tests/files/reduce_exons/empty_sequence_output.fa"
         )
+        self.assertEqualListOfSeqrecords(actual, expected)
 
-    @classmethod
     def test_one_exon(self):
         """reduce_exons.py: test single exon"""
-        fasta_in = "exfi/tests/files/reduce_exons/one_exon_input.fa"
-        fasta_res = "exfi/tests/files/reduce_exons/one_exon_output.fa"
-        self.assertEqualListOfSeqrecords(
-            list(reduce_exons(_fasta_to_list(fasta_in))),
-            _fasta_to_list(fasta_res)
+        actual = list(reduce_exons(_fasta_to_list(
+            "exfi/tests/files/reduce_exons/one_exon_input.fa"
+        )))
+        expected = _fasta_to_list(
+            "exfi/tests/files/reduce_exons/one_exon_output.fa"
         )
+        self.assertEqualListOfSeqrecords(actual, expected)
 
-    @classmethod
     def test_same_exon(self):
         """reduce_exons.py: read the same exon twice"""
-        fasta_in = "exfi/tests/files/reduce_exons/same_exon_input.fa"
-        fasta_res = "exfi/tests/files/reduce_exons/same_exon_output.fa"
-        self.assertEqualListOfSeqrecords(
-            list(reduce_exons(_fasta_to_list(fasta_in))),
-            _fasta_to_list(fasta_res)
+        actual = list(reduce_exons(_fasta_to_list(
+            "exfi/tests/files/reduce_exons/same_exon_input.fa"
+        )))
+        expected = _fasta_to_list(
+            "exfi/tests/files/reduce_exons/same_exon_output.fa"
         )
+        self.assertEqualListOfSeqrecords(actual, expected)
 
-    @classmethod
     def test_different_exons(self):
         """reduce_exons.py: read the same exon twice"""
-        fasta_in = "exfi/tests/files/reduce_exons/different_exons_input.fa"
-        fasta_res = "exfi/tests/files/reduce_exons/different_exons_output.fa"
-        self.assertEqualListOfSeqrecords(
-            list(reduce_exons(_fasta_to_list(fasta_in))),
-            _fasta_to_list(fasta_res)
+        actual = list(reduce_exons(_fasta_to_list(
+            "exfi/tests/files/reduce_exons/different_exons_input.fa"
+        )))
+        expected = _fasta_to_list(
+            "exfi/tests/files/reduce_exons/different_exons_output.fa"
         )
+        self.assertEqualListOfSeqrecords(actual, expected)

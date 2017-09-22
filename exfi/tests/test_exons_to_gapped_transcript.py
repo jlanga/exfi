9ff5c7138e3b2aef8b351f8e70100271cb9ccab5
Jorge Langa <jorge.langa.arranz@gmail.com>
2017-09-22 17:30:15 +0200

    Simpler tests

--- exfi/tests/test_exons_to_gapped_transcript.py
+++ exfi/tests/test_exons_to_gapped_transcript.py
@@ -9,59 +9,76 @@ from exfi.tests.auxiliary_functions import \
     CustomAssertions, \
     _fasta_to_list
 
-def fasta_to_transcript_to_exon_dict(fasta_fn):
+def _fasta_to_transcript_to_exon_dict(fasta_fn):
     """Pipe to read fasta and build dict"""
     exons = _fasta_to_list(fasta_fn)
     return build_transcript_to_exon_dict(exons)
 
+very_simple_dict = {"ENSDART00000161035.1": ("EXON00000000001", )}  # TODO
+simple_dict = {
+    "ENSDART00000161035.1": (
+        "EXON00000000001", "EXON00000000002", "EXON00000000003"
+    )
+}
+harder_dict ={
+    "ENSDART00000161035.1": (
+        "EXON00000000001", "EXON00000000002", "EXON00000000003"
+    ),
+    "ENSDART00000165342.1": (
+        "EXON00000000004", "EXON00000000005", "EXON00000000006",
+        "EXON00000000007", "EXON00000000008", "EXON00000000009",
+        "EXON00000000010", "EXON00000000011", "EXON00000000012",
+        "EXON00000000013", "EXON00000000014", "EXON00000000015"
+    )
+}
+
+
 class TestBuildTranscriptToExonDict(TestCase):
 
     def test_empty_exons(self):
-        actual = build_transcript_to_exon_dict([])
-        expected = {}
-        self.assertEqual(actual, expected)
+        """exons_to_gapped_transcript.py: build the transcript to exon dict
+        given an empty input"""
+        self.assertEqual(build_transcript_to_exon_dict([]), {})
 
     def test_single_exon(self):
-        actual = fasta_to_transcript_to_exon_dict(
-            "exfi/tests/files/exons_to_gapped_transcript/single.fa",
+        """exons_to_gapped_transcript.py: build the transcript to exon dict
+        given a single exon"""
+        self.assertEqual(
+            _fasta_to_transcript_to_exon_dict(
+                "exfi/tests/files/exons_to_gapped_transcript/single.fa",
+            ),
+            very_simple_dict  # TODO
         )
-        expected = {"ENSDART00000161035.1": ("EXON00000000001", )}  # TODO
-        self.assertEqual(actual, expected)
 
     def test_ordered_exons(self):
-        actual = fasta_to_transcript_to_exon_dict(
-            "exfi/tests/files/exons_to_gapped_transcript/ordered.fa",
+        """exons_to_gapped_transcript.py: build the transcript to exon dict
+        given already ordered exons"""
+        self.assertEqual(
+            _fasta_to_transcript_to_exon_dict(
+                "exfi/tests/files/exons_to_gapped_transcript/ordered.fa",
+            ),
+            simple_dict
         )
-        expected = {"ENSDART00000161035.1":
-            ("EXON00000000001", "EXON00000000002", "EXON00000000003")
-        }
-        self.assertEqual(actual, expected)
 
     def test_disordered_exons(self):
-        actual = fasta_to_transcript_to_exon_dict(
-            "exfi/tests/files/exons_to_gapped_transcript/disordered.fa",
+        """exons_to_gapped_transcript.py: build the transcript to exon dict
+        given disordered exons"""
+        self.assertEqual(
+            _fasta_to_transcript_to_exon_dict(
+                "exfi/tests/files/exons_to_gapped_transcript/disordered.fa",
+            ),
+            simple_dict
         )
-        expected = {"ENSDART00000161035.1":
-            ("EXON00000000001", "EXON00000000002", "EXON00000000003")
-        }
-        self.assertEqual(actual, expected)
 
     def test_different_transcripts(self):
-        actual = fasta_to_transcript_to_exon_dict(
-            "exfi/tests/files/exons_to_gapped_transcript/different_transcripts.fa",
-        )
-        expected = {
-            "ENSDART00000161035.1": (
-                "EXON00000000001", "EXON00000000002", "EXON00000000003"
+        """exons_to_gapped_transcript.py: build the transcript to exon dict
+        given exons from different transcripts"""
+        self.assertEqual(
+            _fasta_to_transcript_to_exon_dict(
+                "exfi/tests/files/exons_to_gapped_transcript/different_transcripts.fa",
             ),
-            "ENSDART00000165342.1": (
-                "EXON00000000004", "EXON00000000005", "EXON00000000006",
-                "EXON00000000007", "EXON00000000008", "EXON00000000009",
-                "EXON00000000010", "EXON00000000011", "EXON00000000012",
-                "EXON00000000013", "EXON00000000014", "EXON00000000015"
-            )
-        }
-        self.assertEqual(actual, expected)
+            harder_dict
+        )
 
 
 class TestExonsToGappedTranscript(TestCase, CustomAssertions):

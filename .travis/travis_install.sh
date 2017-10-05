f29ea6b772b5c1a0498890e59faa0768428c2378
Jorge Langa <jorge.langa.arranz@gmail.com>
2017-10-05 14:57:36 +0200

    Switching back to manual building instead of brewº

--- .travis/travis_install.sh
+++ .travis/travis_install.sh
@@ -8,7 +8,23 @@ conda config --add channels r
 conda config --add channels bioconda
 conda install --yes abyss biopython bedtools pandas networkx
 
-brew tap homebrew/science
-brew update
-brew update
-brew install biobloomtools
+
+# SDSL-lite
+# https://hub.docker.com/r/adamnovak/sequence-graphs/~/dockerfile/
+git clone https://github.com/simongog/sdsl-lite.git && \
+pushd sdsl-lite && \
+./install.sh /usr && \
+popd && \
+rm -Rf sdsl-lite
+
+# biobloomtools
+# as in https://github.com/bcgsc/biobloom
+git clone https://github.com/bcgsc/biobloom.git && \
+pushd biobloom/ && \
+git submodule update --init && \
+./autogen.sh && \
+./configure && \
+make -j 2 && \
+make install && \
+popd && \
+rm -rf biobloom/ 

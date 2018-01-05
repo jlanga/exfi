f198a595adae453100e0653ce7549908a01ece20
Jorge Langa <jorge.langa.arranz@gmail.com>
2018-01-05 17:03:20 +0100

    Better unittest output

--- tests/test_find_exons.py
+++ tests/test_find_exons.py
@@ -13,12 +13,12 @@ from tests.auxiliary_functions import \
 class TestProcessOutput(unittest.TestCase):
 
     def test_empty_process(self):
-        """find_exons.py: process an empty stream"""
+        """_command_to_list: process an empty stream"""
         results = _command_to_list(["cat", "/dev/null"])
         self.assertEqual(first=results, second=[])
 
     def test_simple_process(self):
-        """find_exons.py: process an simple stream"""
+        """_command_to_list: process an simple stream"""
         results = _command_to_list(
             ["cat", "tests/files/find_exons/simple.bed"]
         )
@@ -28,7 +28,7 @@ class TestProcessOutput(unittest.TestCase):
         )
 
     def test_big_process(self):
-        """find_exons.py: process an big stream"""
+        """_command_to_list: process an big stream"""
         results = _command_to_list(
             ["cat", "tests/files/find_exons/big.bed"]
         )
@@ -44,7 +44,7 @@ class TestProcessOutput(unittest.TestCase):
 class TestGetFasta(unittest.TestCase, CustomAssertions):
 
     def test_empty_sequence_empty_bed(self):
-        """find_exons.py: process an empty fasta and an empty bed"""
+        """_getfasta_to_list: process an empty fasta and an empty bed"""
         transcriptome_dict={}
         iterable_of_bed=[]
         self.assertEqual(
@@ -53,7 +53,7 @@ class TestGetFasta(unittest.TestCase, CustomAssertions):
         )
 
     def test_empty_sequence_one_bed(self):
-        """find_exons.py: process an empty fasta and an empty bed"""
+        """_getfasta_to_list: process an empty fasta and an empty bed"""
         transcriptome_dict={}
         iterable_of_bed=[("test1", 14, 27)]
         self.assertEqualListOfSeqrecords(
@@ -62,7 +62,7 @@ class TestGetFasta(unittest.TestCase, CustomAssertions):
         )
 
     def test_one_sequence_empty_bed(self):
-        """find_exons.py: process a simple fasta and an empty bed"""
+        """_getfasta_to_list: process a simple fasta and an empty bed"""
         transcriptome_dict = _fasta_to_dict(
             "tests/files/find_exons/single_sequence.fa"
         )
@@ -73,7 +73,7 @@ class TestGetFasta(unittest.TestCase, CustomAssertions):
         )
 
     def test_one_sequence_one_bed(self):
-        """find_exons.py: process an single fasta and a single bed record"""
+        """_getfasta_to_list: process an single fasta and a single bed record"""
         transcriptome_dict = _fasta_to_dict(
             "tests/files/find_exons/one_sequence_one_bed_input.fa"
         )
@@ -86,7 +86,7 @@ class TestGetFasta(unittest.TestCase, CustomAssertions):
         )
 
     def test_multiple_sequences_multiple_beds(self):
-        """find_exons.py: process an multiline fasta and multple bed"""
+        """_getfasta_to_list: process an multiline fasta and multple bed"""
         transcriptome_dict = _fasta_to_dict(
             "tests/files/find_exons/multiple_sequences_multiple_beds_input.fa",
         )
@@ -104,21 +104,21 @@ class TestGetFasta(unittest.TestCase, CustomAssertions):
 class TestFindExonsPipeline(unittest.TestCase):
 
     def test_notranscriptome_noreads(self):
-        """find_exons.py: Process an empty transcriptome and an empty BF"""
+        """_bf_and_process: Process an empty transcriptome and an empty BF"""
         reads_fns = ["/dev/null"]
         transcriptome_fn = "/dev/null"
         results = _bf_and_process(reads_fns, transcriptome_fn)
         self.assertEqual(results, [])
 
     def test_transcriptome_noreads(self):
-        """find_exons.py: Process a small transcriptome and an empty BF"""
+        """_bf_and_process: Process a small transcriptome and an empty BF"""
         reads_fns = ["/dev/null"]
         transcriptome_fn = 'tests/files/find_exons/small_transcriptome.fa'
         results = _bf_and_process(reads_fns, transcriptome_fn)
         self.assertEqual(results, [])
 
     def test_small_data(self):
-        """find_exons.py: Process an empty transcriptome and a small BF"""
+        """_bf_and_process: Process an empty transcriptome and a small BF"""
         reads_fns = [
             'tests/files/find_exons/reads_1.fq',
             'tests/files/find_exons/reads_2.fq'

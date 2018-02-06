aa4a5ccb811a89855e20d74278a9ca3f92ea9a82
Jorge Langa <jorge.langa.arranz@gmail.com>
2018-02-06 17:06:01 +0100

    splice_graph -> splice_graph_dict

--- exfi/io/gfa1_to_splice_graph.py
+++ exfi/io/gfa1_to_splice_graph.py
@@ -8,7 +8,31 @@ import logging
 
 import networkx as nx
 
-from exfi.io.read_gfa1 import read_gfa1
+from exfi.io.read_gfa1 import \
+    read_gfa1
+
+
+def _split_node2coord(node2coord: dict, node2transcript: dict) -> dict:
+    """Split the big node2coord dict into its subcompoenents (transcripts)"""
+    splitted_node2coord = {key: dict() for key in set(node2transcript.values())}
+    for node, coordinates in node2coord.items():
+        transcript = node2transcript[node]
+        if node not in splitted_node2coord[transcript]:
+            splitted_node2coord[transcript][node] = tuple()
+        splitted_node2coord[transcript][node] += coordinates
+    return splitted_node2coord
+
+
+
+def _split_edge2overlap(edge2overlap: dict, node2transcript: dict) -> dict:
+    """Split the big edge2overlap dict into subcomponents (transcripts)"""
+    splitted_edge2overlap = {transcript: dict() for transcript in set(node2transcript.values())}
+    for edge, overlap in edge2overlap.items():
+        transcript = node2transcript[edge[0]]
+        splitted_edge2overlap[transcript][edge] = overlap
+    return splitted_edge2overlap
+
+
 
 def gfa1_to_splice_graph(handle):
     """(str) -> nx.DiGraph
@@ -17,25 +41,44 @@ def gfa1_to_splice_graph(handle):
     """
     logging.info("Converting gfa1 %s to splice graph", handle)
 
-    # Read
+    # Read and process
+    logging.info("\tReading and processing GFA file %s", handle)
     gfa1 = read_gfa1(handle)
+    node2coord = gfa1["containments"]
+    edge2overlap = gfa1["links"]
+    transcript2nodes = gfa1["paths"]
 
-    # Process
-    logging.info("\tProcessing coordinates")
-    coordinate_dict = gfa1["containments"]
-    logging.info("\tProcessing links")
-    overlap_dict = gfa1["links"]
+    # Revert path2nodes
+    node2transcript = {
+        value: key
+        for key, values in transcript2nodes.items()
+        for value in values
+    }
 
-    # Build
-    logging.info("\tBuilding splice graph")
-    splice_graph = nx.DiGraph()
-    splice_graph.add_nodes_from(coordinate_dict.keys())
-    splice_graph.add_edges_from(overlap_dict.keys())
-    nx.set_node_attributes(
-        G=splice_graph, name="coordinates", values=coordinate_dict
-    )
-    nx.set_edge_attributes(
-        G=splice_graph, name="overlaps", values=overlap_dict
-    )
+    # Split node2coord
+    transcript2node2coord = _split_node2coord(node2coord, node2transcript)
+    transcript2edge2overlap = _split_edge2overlap(edge2overlap, node2transcript)
 
-    return splice_graph
+    # Initialize
+    splice_graph_dict = {transcript: None for transcript in transcript2nodes}
+
+    # process
+    for transcript in splice_graph_dict.keys():
+        splice_graph = nx.DiGraph()
+
+        node2coord = transcript2node2coord[transcript]
+        splice_graph. add_nodes_from(node2coord.keys())
+        nx.set_node_attributes(
+            G=splice_graph, name="coordinates", values=node2coord
+        )
+
+        edge2overlap = transcript2edge2overlap[transcript]
+        splice_graph.add_edges_from(edge2overlap.keys())
+        nx.set_edge_attributes(
+            G=splice_graph, name="overlaps", values=edge2overlap
+        )
+
+        splice_graph_dict[transcript] = splice_graph
+
+
+    return splice_graph_dict

5806927b14bd1221c5fdfe73166b9b7462928b22
Jorge Langa <jorge.langa.arranz@gmail.com>
2018-02-28 14:55:20 +0100

    Added pipeline scripts for fast inspection

---
+++ scripts/pipeline_chr25.sh
@@ -0,0 +1,97 @@
+#!/usr/bin/env bash
+set -euxo pipefail
+
+mkdir -p results/
+
+k=31
+m=4G
+l=1
+
+if [ ! -f results/drer25sim_k${k}_m${m}_l${l}.bloom ]; then
+build_baited_bloom_filter \
+    --input-fasta data/Danio_rerio.GRCz10.cdna.25.fa \
+    --kmer $k \
+    --bloom-size $m \
+    --levels $l \
+    --threads 4 \
+    --output-bloom results/drer25sim_k${k}_m${m}_l${l}.bloom \
+    data/drer25.1_1.fq.gz \
+    data/drer25.1_2.fq.gz \
+    data/drer25.2_1.fq.gz \
+    data/drer25.2_2.fq.gz
+fi
+
+if [ ! -f results/drer25real_k${k}_m${m}_l1.bloom ]; then
+build_baited_bloom_filter \
+    --input-fasta data/Danio_rerio.GRCz10.cdna.25.fa \
+    --kmer $k \
+    --bloom-size $m \
+    --levels 1 \
+    --threads 4 \
+    --output-bloom results/drer25real_k${k}_m${m}_l1.bloom \
+    data/Danio_rerio.GRCz10.dna.25.fa.gz
+fi
+
+build_splice_graph \
+    --input-fasta data/Danio_rerio.GRCz10.cdna.all.fa \
+    --input-bloom results/drer25sim_k${k}_m${m}_l${l}.bloom \
+    --kmer $k \
+    --max-fp-bases 3 \
+    --output-gfa results/drer25sim.gfa \
+    --verbose \
+    --threads 4
+
+build_splice_graph \
+    --input-fasta data/Danio_rerio.GRCz10.cdna.all.fa \
+    --input-bloom results/drer25sim_k${k}_m${m}_l${l}.bloom \
+    --kmer $k \
+    --max-fp-bases 3 \
+    --correct \
+    --output-gfa results/drer25sim_correct.gfa \
+    --verbose \
+    --threads 4
+
+build_splice_graph \
+    --input-fasta data/Danio_rerio.GRCz10.cdna.all.fa \
+    --input-bloom results/drer25sim_k${k}_m${m}_l${l}.bloom \
+    --kmer $k \
+    --max-fp-bases 3 \
+    --correct \
+    --polish \
+    --output-gfa results/drer25sim_correct_polish.gfa \
+    --verbose \
+    --threads 4
+
+build_splice_graph \
+    --input-fasta data/Danio_rerio.GRCz10.cdna.all.fa \
+    --input-bloom results/drer25sim_k${k}_m${m}_l${l}.bloom \
+    --kmer $k \
+    --max-fp-bases 3 \
+    --correct \
+    --polish \
+    --collapse \
+    --output-gfa results/drer25sim_correct_polish_collapse.gfa \
+    --verbose \
+    --threads 4
+
+build_splice_graph \
+    --input-fasta data/Danio_rerio.GRCz10.cdna.all.fa \
+    --input-bloom results/drer25real_k${k}_m${m}_l1.bloom \
+    --kmer $k \
+    --max-fp-bases 3 \
+    --correct \
+    --output-gfa results/drer25real.gfa \
+    --verbose \
+    --threads 4
+
+gfa1_to_exons \
+    --input-gfa results/drer25sim.gfa \
+    --output-fasta results/drer25sim_exons.fa \
+    --soft-mask-overlaps
+
+gfa1_to_exons \
+    --input-gfa results/drer25real.gfa \
+    --output-fasta results/drer25real_exons.fa \
+    --soft-mask-overlaps
+
+bash src/pr_chr25.sh
---
+++ scripts/pipeline_test.sh
@@ -0,0 +1,71 @@
+#!/usr/bin/env bash
+set -euxo pipefail
+
+
+mkdir -p results/
+
+if [ ! -f results/test_k27_m500M_l1.bloom ]; then
+    build_baited_bloom_filter \
+        --input-fasta data/transcript.fa \
+        --kmer-size 27 \
+        --bloom-size 500M \
+        --levels 1 \
+        --threads 4 \
+        --output-bloom results/test_k27_m500M_l1.bloom \
+        data/genome.fa.gz
+fi
+
+build_splice_graph \
+    --input-fasta data/transcript.fa \
+    --input-bloom results/test_k27_m500M_l1.bloom \
+    --kmer-size 27 \
+    --max-fp-bases 5 \
+    --output-gfa results/test.gfa \
+    --verbose \
+    --threads 4
+
+build_splice_graph \
+    --input-fasta data/transcript.fa \
+    --input-bloom results/test_k27_m500M_l1.bloom \
+    --kmer-size 27 \
+    --max-fp-bases 5 \
+    --output-gfa results/test_correct.gfa \
+    --verbose \
+    --threads 4 \
+    --correct
+
+build_splice_graph \
+    --input-fasta data/transcript.fa \
+    --input-bloom results/test_k27_m500M_l1.bloom \
+    --kmer-size 27 \
+    --max-fp-bases 5 \
+    --output-gfa results/test_correct_polish.gfa \
+    --verbose \
+    --threads 4 \
+    --correct \
+    --polish
+
+build_splice_graph \
+    --input-fasta data/transcript.fa \
+    --input-bloom results/test_k27_m500M_l1.bloom \
+    --kmer-size 27 \
+    --max-fp-bases 5 \
+    --output-gfa results/test_correct_polish_collapse.gfa \
+    --verbose \
+    --threads 4 \
+    --correct \
+    --polish \
+    --collapse
+
+
+gfa1_to_exons \
+    --input-gfa results/test.gfa \
+    --output-fasta results/test_exons.fa \
+    --soft-mask-overlaps \
+    --verbose
+
+gfa1_to_gapped_transcripts \
+    --input-gfa results/test.gfa \
+    --output-fasta results/test_gapped.fa \
+    --hard-mask-overlaps \
+    --verbose

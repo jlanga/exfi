07be0a4e45f831bf42cb97ca025135caa7fa1346
Jorge Langa <jorge.langa.arranz@gmail.com>
2019-01-16 19:02:28 +0100

    masking + tests

--- exfi/io/masking.py
+++ exfi/io/masking.py
@@ -6,91 +6,95 @@ exfi.io.masking.py: submodule to soft and hard mask sequence strings
 
 import logging
 
+import pandas as pd
 
-def _process_overlap_cigar(cigar_string: str) -> list:
-    """Process a simple CIGAR string.
+def soft_mask(sequence, left, right):
+    """Lowercase the first left bases and last right bases of sequence
 
-    :param cigar_string: Process a simple CIGAR string of the shape number-letter: 90G, 10M, ...
+    >>> soft_mask('ACCGATCGATCGTAG', 2, 1)
+    'acCGATCGATCGTAg'
+    >>> soft_mask('ACCGATCGATCGTAG', 0, 2)
+    'ACCGATCGATCGTag'
+    >>> soft_mask('ACCGATCGATCGTAG', 2, 0)
+    'acCGATCGATCGTAG'
+    >>> soft_mask('ACCGATCGATCGTAG', 0, 0)
+    'ACCGATCGATCGTAG'
     """
-    return [cigar_string[-1], int(cigar_string[:-1])]
+    if left == 0 and right == 0:
+        return sequence
+    if left == 0 and right > 0:
+        return sequence[:-right] + sequence[-right:].lower()
+    if left > 0 and right == 0:
+        return sequence[:left].lower() + sequence[left:]
+    return  sequence[:left].lower() + sequence[left:-right] + sequence[-right:].lower()
 
 
-def _soft_mask_right(string: str, n_bases: int) -> str:
-    """Soft mask the rightmost n bases.
 
-    :param str string: string of nucleotides to be soft masked.
-    :param int n_bases: number of letters at the right end (3') to be soft masked.
+def hard_mask(sequence, left, right):
+    """Mask with N the first left bases and last right bases of sequence
+
+    >>> hard_mask('ACCGATCGATCGTAG', 2, 1)
+    'NNCGATCGATCGTAN'
+    >>> hard_mask('ACCGATCGATCGTAG', 0, 2)
+    'ACCGATCGATCGTNN'
+    >>> hard_mask('ACCGATCGATCGTAG', 2, 0)
+    'NNCGATCGATCGTAG'
+    >>> hard_mask('ACCGATCGATCGTAG', 0, 0)
+    'ACCGATCGATCGTAG'
     """
-    return string[:-n_bases] + string[-n_bases:].lower()
+    if left == 0 and right == 0:
+        return sequence
+    if left == 0 and right > 0:
+        return sequence[:-right] + 'N' * right
+    if left > 0 and right == 0:
+        return 'N' * left + sequence[left:]
+    return 'N' * left + sequence[left:-right] + 'N' * right
 
 
-def _soft_mask_left(string: str, n_bases: int) -> str:
-    """Soft mask the leftmost n bases.
 
-    :param str string: string of nucleotides to be soft masked
-    :param int n_bases: number of letters at the left end (5') to be soft masked.
-    """
-    return string[:n_bases].lower() + string[n_bases:]
-
-
-def _soft_mask(exon_dict, overlap_dict):
-    """Soft mask all overlaps in the exon_dict.
-
-    :param dict exon_dict: dict of exon_id: sequence
-    :param dict overlap_dict: dict of (node1, node2): overlap
-    """
-    exon_dict = exon_dict.copy()
-    for (start, end), overlap in overlap_dict.items():
-        if overlap > 0:
-            exon_dict[start] = _soft_mask_right(exon_dict[start], overlap)
-            exon_dict[end] = _soft_mask_left(exon_dict[end], overlap)
-    return exon_dict
-
-
-def _hard_mask_right(string: str, n_bases: int) -> str:
-    """Hard mask the rightmost n_bases bases
-
-    :param string: Nucleotide sequence to hard mask.
-    :param n_bases: Number of bases to hard mask at the right (3') end.
-    """
-    return string[:-n_bases] + "N" * n_bases
-
-
-def _hard_mask_left(string: str, n_bases: int):
-    """Hard mask the leftmost n_bases bases
-
-    :param str string: Nucleotide sequence to hard mask.
-    :param int n_bases: Number of bases to hard mask at the left (5') end.
-    """
-    return "N" * n_bases + string[n_bases:]
-
-
-def _hard_mask(exon_dict, overlap_dict):
-    """Hard mask all overlaps in the exon_dict.
-
-    :param dict exon_dict: Dict of the shape exon_id: sequence.
-    :param dict overlap_dict: Dict of the shape (exon1, exon2): overlap between them.
-    """
-    exon_dict = exon_dict.copy()
-    for (start, end), overlap in overlap_dict.items():
-        if overlap > 0:
-            exon_dict[start] = _hard_mask_right(exon_dict[start], overlap)
-            exon_dict[end] = _hard_mask_left(exon_dict[end], overlap)
-    return exon_dict
-
-
-def _mask(exon_dict, overlap_dict, masking: str = "none"):
+def mask(node2sequence, edge2overlap, masking: str = "none"):
     """If any of the soft mask or hard mask are activated, mask
 
     :param dict exon_dict: Dict of the shape exon_id: sequence.
     :param dict overlap_dict: Dict of the shape (exon1, exon2): overlap between them.
-    :param str masking: Type of masking to apply. Options: hard, soft, none (Default value = "None")
-    .
+    :param str masking: Type of masking to apply. Options: hard, soft, none
+     (Default value = "None")    .
     """
+    if masking == 'none':
+        return node2sequence
+
+    edge2overlap['tmp_overlap'] = edge2overlap.overlap.map(
+        lambda x: x if x > 0 else 0
+    )
+
+    tmp = pd.merge(
+        node2sequence,
+        edge2overlap[['u', 'tmp_overlap']].rename(
+            columns={'u': 'name', 'tmp_overlap': 'mask_right'}
+        ),
+        on=['name']
+    )
+
+    complete = complete = pd.merge(
+        tmp,
+        edge2overlap[['v', 'tmp_overlap']].rename(
+            columns={'v': 'name', 'tmp_overlap': 'mask_left'}
+        ),
+        on=['name']
+        )
+
+    complete['tmp'] = tuple(zip(
+        complete.sequence, complete.mask_left, complete.mask_right
+    ))
+
+
+
     if masking == "hard":
         logging.info("\tHard masking sequences")
-        exon_dict = _hard_mask(exon_dict, overlap_dict)
+        complete['sequence'] = complete.tmp.map(lambda x: hard_mask(*x))
     elif masking == "soft":
         logging.info("\tSoft masking sequences")
-        exon_dict = _soft_mask(exon_dict, overlap_dict)
+        complete['sequence'] = complete.tmp.map(lambda x: soft_mask(*x))
+
+    exon_dict = complete[['name', 'sequence']].reset_index(drop=True)
     return exon_dict
--- tests/test_io/test_masking.py
+++ tests/test_io/test_masking.py
@@ -7,133 +7,33 @@ Tests for exfi.io.masking
 from unittest import TestCase, main
 
 from exfi.io.masking import \
-    _process_overlap_cigar, \
-    _soft_mask_right, \
-    _soft_mask_left, \
-    _soft_mask, \
-    _hard_mask_right, \
-    _hard_mask_left, \
-    _hard_mask, \
-    _mask
+    mask
 
-from tests.data import \
-    OVERLAPS_COMPLEX
-
-# pylint: disable=no-name-in-module
-from tests.test_io.test_gfa1_to_exons import \
-    EXONS_COMPLEX_DICT, EXONS_COMPLEX_SOFT_DICT, EXONS_COMPLEX_HARD_DICT
-
-from tests.custom_assertions import CustomAssertions
-
-
-class TestProcessOverlapCigar(TestCase):
-    """Tests for exfi.io.masking._process_overlap_cigar"""
-
-    def test_empty(self):
-        """exfi.io.masking._process_overlap_cigar: empty case"""
-        with self.assertRaises(IndexError):
-            _process_overlap_cigar("")
-
-    def test_wrong(self):
-        """exfi.io.masking._process_overlap_cigar: wrong case"""
-        with self.assertRaises(IndexError):
-            _process_overlap_cigar("")
-
-    def test_correct(self):
-        """exfi.io.masking._process_overlap_cigar: correct case"""
-        self.assertEqual(
-            _process_overlap_cigar("13M"),
-            ["M", 13]
-        )
+from tests.io.bed import \
+    EDGE2OVERLAP_COMPLEX, NODE2SEQUENCE_COMPLEX, NODE2SEQUENCE_COMPLEX_HARD, \
+    NODE2SEQUENCE_COMPLEX_SOFT
 
 
 
-class TestSoftMaskRight(TestCase):
-    """Tests for exfi.io.masking._soft_mask_right"""
-
-    def test_soft_mask_right(self):
-        """exfi.io.masking._soft_mask_right: simple"""
-        actual = _soft_mask_right("AAAAA", 3)
-        expected = "AAaaa"
-        self.assertEqual(actual, expected)
-
-
-
-class TestSoftMaskLeft(TestCase):
-    """Tests for exfi.io.masking._soft_mask_left"""
-
-    def test_soft_mask_left(self):
-        """exfi.io.masking._soft_mask_left: simple"""
-        actual = _soft_mask_left("AAAAA", 3)
-        expected = "aaaAA"
-        self.assertEqual(actual, expected)
-
-
-
-class TestSoftMask(TestCase, CustomAssertions):
-    """Tests for exfi.io.masking._soft_mask"""
-
-    def test_soft_mask(self):
-        """exfi.io.masking._soft_mask: simple"""
-        actual = _soft_mask(EXONS_COMPLEX_DICT, OVERLAPS_COMPLEX)
-        expected = EXONS_COMPLEX_SOFT_DICT
-        self.assertEqualDict(actual, expected)
-
-
-
-class TestHardMaskRight(TestCase):
-    """Tests for exfi.io.masking._hard_mask_right"""
-
-    def test_hard_mask_right(self):
-        """exfi.io.masking._hard_mask_right: simple"""
-        actual = _hard_mask_right("AAAAA", 3)
-        expected = "AANNN"
-        self.assertEqual(actual, expected)
-
-
-
-class TestHardMaskLeft(TestCase):
-    """Tests for exfi.io.masking._hard_mask_left"""
-
-    def test_hard_mask_left(self):
-        """exfi.io.masking._hard_mask_left: simple"""
-        actual = _hard_mask_left("AAAAA", 3)
-        expected = "NNNAA"
-        self.assertEqual(actual, expected)
-
-
-
-class TestHardMask(TestCase, CustomAssertions):
-    """Tests for exfi.io.masking._hard_mask"""
-
-    def test_hard_mask(self):
-        """exfi.io.masking._hard_mask: simple"""
-        actual = _hard_mask(EXONS_COMPLEX_DICT, OVERLAPS_COMPLEX)
-        expected = EXONS_COMPLEX_HARD_DICT
-        self.assertEqualDict(actual, expected)
-
-
-
-class TestMask(TestCase, CustomAssertions):
+class TestMask(TestCase):
     """Tests for exfi.io.masking._mask"""
 
     def test_no_mask(self):
         """exfi.io.masking._mask: no masking"""
-        actual = _mask(EXONS_COMPLEX_DICT, OVERLAPS_COMPLEX, "none")
-        expected = EXONS_COMPLEX_DICT
-        self.assertEqualDict(actual, expected)
+        observed = mask(NODE2SEQUENCE_COMPLEX, EDGE2OVERLAP_COMPLEX, "none")
+        self.assertTrue(observed.equals(NODE2SEQUENCE_COMPLEX))
 
     def test_soft_mask(self):
         """exfi.io.masking._mask: soft masking"""
-        actual = _mask(EXONS_COMPLEX_DICT, OVERLAPS_COMPLEX, "soft")
-        expected = EXONS_COMPLEX_SOFT_DICT
-        self.assertEqualDict(actual, expected)
+        observed = mask(NODE2SEQUENCE_COMPLEX, EDGE2OVERLAP_COMPLEX, "soft")
+        print(observed.values.tolist())
+        self.assertTrue(observed.equals(NODE2SEQUENCE_COMPLEX_SOFT))
 
     def test_hard_mask(self):
         """exfi.io.masking._mask: hard masking"""
-        actual = _mask(EXONS_COMPLEX_DICT, OVERLAPS_COMPLEX, "hard")
-        expected = EXONS_COMPLEX_HARD_DICT
-        self.assertEqualDict(actual, expected)
+        observed = mask(NODE2SEQUENCE_COMPLEX, EDGE2OVERLAP_COMPLEX, "hard")
+        print(observed.values.tolist())
+        self.assertTrue(observed.equals(NODE2SEQUENCE_COMPLEX_HARD))
 
 
 

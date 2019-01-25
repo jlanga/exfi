f00009128637496c7b53e0329857fa314b4af339
Jorge Langa <jorge.langa.arranz@gmail.com>
2019-01-25 15:29:40 +0100

    Universal gfa1 reader

--- exfi/io/gfa1_to_bed.py
+++ exfi/io/gfa1_to_bed.py
@@ -3,33 +3,24 @@
 """exfi.io.gfa1_to_bed.py: submodule to read a GFA1 file and convert it to BED4
 """
 
-import pandas as pd
 import numpy as np
 
 from exfi.io.bed import BED4_COLS, BED4_DTYPES
+from exfi.io.read_gfa import read_gfa1
 
 def gfa1_to_bed4(filename):
     """Read a GFA1 file and convert it to BED4"""
 
-    with open(filename, "r") as gfa:
-        containments = pd.DataFrame(
-            data=[
-                x.strip().split("\t") for x in gfa.readlines() if x[0] == "C"
-            ],
-            columns=["RecordType", "Container", "ContainerOrient", "Contained",
-                     "ContainedOrient", "Pos", "Overlap"],
-            dtype=None
-        )\
-        .astype(dtype={"Pos": np.int})
+    containments = read_gfa1(filename)['containments']
 
-    containments = containments.rename({
-        "Container": "chrom",
-        "Contained": "name"
-    }, axis=1)
-    containments["Overlap"] = containments["Overlap"]\
-        .map(lambda x: np.int(x[:-1]))
-    containments["chrom_start"] = containments["Pos"]
-    containments["chrom_end"] = containments["Pos"] + containments["Overlap"]
+    containments = containments.rename(columns={
+        "container": "chrom",
+        "contained": "name"
+    })
+    containments["overlap"] = containments\
+        .overlap.map(lambda x: np.int(x[:-1]))
+    containments["chrom_start"] = containments["pos"]
+    containments["chrom_end"] = containments["pos"] + containments["overlap"]
     containments = containments[BED4_COLS]
     containments = containments.astype(BED4_DTYPES)
     return containments
--- exfi/io/gfa1_to_fasta.py
+++ exfi/io/gfa1_to_fasta.py
@@ -3,8 +3,6 @@
 """exfi.io.gfa1_to_exons.py: submodule to read a gfa1, extract the exons and
 store it in fasta format"""
 
-import pandas as pd
-
 from exfi.io.read_gfa import read_gfa1
 from exfi.io.masking import mask, cigar_to_int
 
@@ -14,31 +12,11 @@ def gfa1_to_exons(fasta_out, gfa1_in, masking='none'):
 
         gfa1 = read_gfa1(gfa1_in)
 
-        # data = [
-        #     x.strip().split("\t")
-        #     for x in gfa.readlines() if x[0] in set(["S", "L"])
-        # ]
-
-        # if not data:
-        #     return
-
-        # node2sequence = pd.DataFrame(
-        #     data=[x[0:3] for x in data if x[0] == "S"],
-        #     columns=["RecordType", "name", "sequence"]
-        # ).drop(columns="RecordType")
-
         node2sequence = gfa1['segments']\
             .drop(columns='record_type')
 
         if node2sequence.shape[0] == 0:
             return
-
-        # edge2overlap = pd.DataFrame(
-        #     data=[x[0:6] for x in data if x[0] == 'L'],
-        #     columns=["RecordType", "u", "FromOrient", "v", "ToOrient",
-        #              "OverlapCigar"]
-        # ).drop(columns=["RecordType", "FromOrient", "ToOrient"])
-        # edge2overlap["overlap"] = edge2overlap.OverlapCigar.map(cigar_to_int)
 
         edge2overlap = gfa1['links']\
             .drop(columns=['record_type', 'from_orient', 'to_orient'])\
@@ -66,44 +44,30 @@ def gfa1_to_gapped_transcripts(
         fasta_out, gfa1_in, gap_size=100, masking='none'):
     """Convert a GFA1 file to a gapped transcript file"""
 
-    with open(gfa1_in, "r") as gfa, open(fasta_out, "w") as fasta:
+    with open(fasta_out, "w") as fasta:
 
         separator = gap_size * 'N'
 
-        # Read only segments and paths
-        data = [
-            x.strip().split("\t")
-            for x in gfa.readlines() if x[0] in set(["S", "P", "L"])
-        ]
-
-        if not data:
-            return
+        gfa1 = read_gfa1(gfa1_in)
 
         # Segments -> node2sequence
-        node2sequence = pd.DataFrame(
-            data=[x[0:3] for x in data if x[0] == "S"],
-            columns=["RecordType", "name", "sequence"],
-        )\
-        .drop(columns="RecordType")
+        node2sequence = gfa1['segments']\
+            .drop(columns=["record_type", 'length'])
 
-        # Links -> edge2overlap
-        edge2overlap = pd.DataFrame(
-            data=[x[0:6] for x in data if x[0] == 'L'],
-            columns=["RecordType", "u", "FromOrient", "v", "ToOrient",
-                     "OverlapCigar"]
-        ).drop(columns=["RecordType", "FromOrient", "ToOrient"])
-        edge2overlap["overlap"] = edge2overlap.OverlapCigar.map(cigar_to_int)
+        if node2sequence.shape[0] == 0:
+            return
 
-        # Paths -> path2nodes
-        path2nodes = pd.DataFrame(
-            data=[x[0:4] for x in data if x[0] == "P"],
-            columns=["RecordType", "PathName", "SegmentNames", "Overlaps"]
-            )\
-            .drop(columns=["RecordType", "Overlaps"])
-        path2nodes["SegmentNames"] = path2nodes["SegmentNames"]\
-            .str.replace("+", "")
+        edge2overlap = gfa1['links']\
+            .drop(columns=['record_type', 'from_orient', 'to_orient'])\
+            .rename(columns={
+                'from': 'u', 'to': 'v', 'overlap': 'overlap_cigar'
+            })
+        edge2overlap["overlap"] = edge2overlap.overlap_cigar.map(cigar_to_int)
 
-        del data
+        path2nodes = gfa1['paths']\
+            .drop(columns=['record_type', 'overlaps'])
+        path2nodes.segment_names = path2nodes\
+            .segment_names.str.replace('+', '')
 
         # Mask the sequences
         node2sequence = mask(
@@ -118,13 +82,14 @@ def gfa1_to_gapped_transcripts(
 
         # Compose the sequence
         path2nodes["gapped_sequence"] = path2nodes\
-            .SegmentNames\
+            .segment_names\
             .str.split(',')\
             .map(lambda x: separator.join([node2sequence_dict[y] for y in x]))
 
         # Create the fasta line
         path2nodes["fasta"] = \
-            ">" + path2nodes.PathName + " " + path2nodes.SegmentNames + "\n" + \
+            ">" + path2nodes.path_name + " " + path2nodes.segment_names + \
+            "\n" + \
             path2nodes.gapped_sequence
 
         # Dump everything
